046f0b2020e6594fb06a6711b90d3d2b27c10ff1
Atul Yadav <atulyadav9039ay@gmail.com>
2025-04-22 01:09:07 +0530

    Modify rswag and add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,14 @@
-# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # For production, replace with your frontend domain
+    # Use different origins for different environments
+    origins = if Rails.env.production?
+                ENV.fetch('FRONTEND_PRODUCTION_URL', 'https://your-production-frontend.com')
+              else
+                ENV.fetch('FRONTEND_DEV_URL', 'http://localhost:3000')
+              end
+
+    origins origins
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,16 +1,9 @@
 Rswag::Ui.configure do |c|
-
-  # List the Swagger endpoints that you want to be documented through the
-  # swagger-ui. The first parameter is the path (absolute or relative to the UI
-  # host) to the corresponding endpoint and the second is a title that will be
-  # displayed in the document selector.
-  # NOTE: If you're using rspec-api to expose Swagger files
-  # (under openapi_root) as JSON or YAML endpoints, then the list below should
-  # correspond to the relative paths for those endpoints.
-
+  # Define the Swagger endpoints for UI display.
+  # NOTE: This will be renamed to `openapi_endpoint` in Rswag v3.0
   c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
 
-  # Add Basic Auth in case your API is private
+  # Enable basic authentication if required (e.g., for private APIs).
   # c.basic_auth_enabled = true
   # c.basic_auth_credentials 'username', 'password'
 end
