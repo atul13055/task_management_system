ed5f3e3564b675883a506ecab4e3d71cc602a22f
Atul Yadav <atulyadav9039ay@gmail.com>
2025-04-22 17:46:30 +0530

    config sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,17 @@
-# config/initializers/sidekiq.rb
-
 redis_url = ENV.fetch('REDIS_URL') {
   Rails.env.production? ? raise("REDIS_URL is not set") : 'redis://localhost:6379/0'
 }
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = {
+    url: redis_url,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
 end
-
